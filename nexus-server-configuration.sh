f1e6ac57147c272466e49fbc6825bd7717ddbe1c
Ajay Kumar Kondaveeti <155078540+ajaykondaveeti12@users.noreply.github.com>
2024-06-18 17:34:55 +0530

    Update nexus-server-configuration.sh

--- nexus-server-configuration.sh
+++ nexus-server-configuration.sh
@@ -1,12 +1,11 @@
 Nexus Server Configuration 
 
 sudo yum update -y
-yum install wget vim unzip -y
-sudo  yum install tar make unzip wget vim -y
+yum install wget vim tar make unzip -y
 cd /opt/
 
 #for java11:
-yum fontconfig install java-17-openjdk-devel -y
+yum install fontconfig  java-11-openjdk-devel -y
 java -version
 wget https://download.sonatype.com/nexus/3/nexus-3.69.0-02-java11-unix.tar.gz
 tar -xvf nexus-3.69.0-02-java11-unix.tar.gz
@@ -18,6 +17,7 @@ sudo chown -R nexus:nexus /opt/sonatype-work/
 sudo vim /opt/nexus/bin/nexus.rc
 sudo vim /opt/nexus/bin/nexus.vmoptions  //no need to change
 sudo ln -s /opt/nexus/bin/nexus /etc/rc.d/init.d/nexus
+yum install chkconfig -y
 sudo chkconfig --add nexus
 sudo chkconfig --levels 35 nexus on
 sudo service nexus start
